ab2e77ff8fcb99e6feca57f896a3493803a1d94d
Alexandr <a.avdeev@mail.ru>
2020-09-13 16:47:16 +0300

    Рефакторинг компонента Routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,20 +10,26 @@ function useRouter() {
   return useContext(__RouterContext);
 }
 
+const animationsValues = {
+  from: {
+    opacity: 0.5,
+    transform: "scale(0.5)",
+  },
+  enter: { opacity: 1, transform: "scale(1)" },
+  leave: {
+    opacity: 0,
+    transform: "scale(0.5)",
+  },
+};
+
 const Routes = () => {
   const { location } = useRouter();
 
-  const transitions = useTransition(location, (location) => location.key, {
-    from: {
-      opacity: 0.5,
-      transform: "scale(0.5)",
-    },
-    enter: { opacity: 1, transform: "scale(1)" },
-    leave: {
-      opacity: 0,
-      transform: "scale(0.5)",
-    },
-  });
+  const transitions = useTransition(
+    location,
+    (location) => location.key,
+    animationsValues
+  );
 
   return transitions.map(({ item, props: transition, key }) => (
     <animated.div key={key} style={transition}>
