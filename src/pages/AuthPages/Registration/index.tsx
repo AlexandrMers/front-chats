bbb3d6d72329e6b83c9a37d2f27ceb78bf5e852e
Alexandr <a.avdeev@mail.ru>
2020-09-16 06:31:06 +0300

    Подключил формик. Добавил простейшую валидацию почты при регистрации.

--- src/pages/AuthPages/Registration/index.tsx
+++ src/pages/AuthPages/Registration/index.tsx
@@ -16,9 +16,54 @@ import {
 } from "@ant-design/icons";
 
 import SuccessNotifyRegistration from "./SuccessNotifyRegistration";
+import { useFormik } from "formik";
 
 const RegistrationPage = () => {
-  const [isSuccessRegistration, setIsSuccessRegistration] = useState(false);
+  const {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    values,
+    errors,
+    touched,
+    dirty,
+  } = useFormik<{
+    email: "";
+    password: "";
+  }>({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validate: (values) => {
+      const errors: {
+        [key: string]: string;
+      } = {};
+
+      if (!values.email) {
+        errors.email = "Обязательное поле";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Невалидный адрес почты";
+      }
+
+      if (!values.password) {
+        errors.password = "Обязательное поле";
+      } else if (
+        !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i.test(values.password)
+      ) {
+        errors.password = "Невалидный пароль";
+      }
+
+      return errors;
+    },
+  });
+
+  const [isSuccessRegistration] = useState(false);
 
   return (
     <Wrapper className={styleModule.wrapperAuth}>
@@ -33,16 +78,24 @@ const RegistrationPage = () => {
         <SuccessNotifyRegistration />
       ) : (
         <WhiteBlock className={styleModule.wrapperAuth__form} withShadow>
-          <Form className={styleModule.authForm__form} name="authForm">
+          <Form
+            onSubmitCapture={handleSubmit}
+            className={styleModule.authForm__form}
+            name="authForm"
+          >
             <Form.Item
-              hasFeedback
               className={styleModule.wrapperAuth__input}
               name="email"
+              help={dirty && touched.email && errors.email}
             >
               <Input
+                id="email"
                 type="email"
-                placeholder="E-mail"
+                placeholder="email"
                 prefix={<MailOutlined style={{ opacity: 0.5 }} />}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
               />
             </Form.Item>
 
@@ -60,11 +113,16 @@ const RegistrationPage = () => {
             <Form.Item
               className={styleModule.wrapperAuth__input}
               name="password"
+              help={dirty && touched.password && errors.password}
             >
               <Input
+                id="password"
                 type="password"
                 placeholder="Пароль"
+                onChange={handleChange}
+                onBlur={handleBlur}
                 prefix={<SecurityScanOutlined style={{ opacity: 0.5 }} />}
+                value={values.password}
               />
             </Form.Item>
 
@@ -83,7 +141,6 @@ const RegistrationPage = () => {
               className={styleModule.authForm__button}
               type="primary"
               htmlType="submit"
-              onClick={() => setIsSuccessRegistration(true)}
             >
               Зарегистрироваться
             </Button>
