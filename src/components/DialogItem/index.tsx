2c73d50b83dada8c1975b7b02be961e91becd6fe
Alexandr <a.avdeev@mail.ru>
2020-09-19 18:23:03 +0300

    Сделал первичную верстку компонента DialogItem

--- src/components/DialogItem/index.tsx
+++ src/components/DialogItem/index.tsx
@@ -1,25 +1,36 @@
 import React, { FC, memo } from "react";
 import { Typography } from "antd";
 
+import styleModule from "./style.module.scss";
+
 import Avatar from "primitives/Avatar";
+import classNames from "classnames";
 
 const DialogItem: FC<any> = () => {
   return (
-    <div className="dialogItem">
+    <div className={styleModule.dialogItem}>
       <Avatar
         size={40}
         avatar="https://live.staticflickr.com/65535/50339368162_ddc568be3e_c.jpg"
         name="Светлана"
+        className={classNames(styleModule.dialogItem__avatar)}
       />
-      <div className="dialogItem__content">
-        <header className="dialogItem__header">
-          <Typography.Title level={5}>Светлана</Typography.Title>
-          <time>Сейчас</time>
+      <div className={styleModule.dialogItem__content}>
+        <header className={styleModule.dialogItem__header}>
+          <Typography.Title className={styleModule.dialogItem__title} level={5}>
+            Светлана
+          </Typography.Title>
+          <time className={styleModule.dialogItem__date}>Сейчас</time>
         </header>
 
-        <section className="dialogItem__msgInfo">
-          <Typography.Paragraph ellipsis>
-            Текст последнего сообщения...
+        <section className={styleModule.dialogItem__msgInfo}>
+          <Typography.Paragraph
+            ellipsis
+            className={styleModule.dialogItem__messageText}
+          >
+            Текст последнего сообщения Текст последнего сообщения Текст
+            последнего сообщения Текст последнего сообщения Текст последнего
+            сообщения
           </Typography.Paragraph>
           <div className="dialogItem__counter">3</div>
         </section>
