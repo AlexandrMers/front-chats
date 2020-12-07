dfe1a38159bd39c2fbad01b92d24265f6f7bb150
alexandr <bratvailolco@mail.ru>
2020-12-07 06:39:38 +0300

    Добавлен функционал отображения прикрепленных файлов.

--- src/components/InputMessage/index.tsx
+++ src/components/InputMessage/index.tsx
@@ -6,6 +6,8 @@ import React, {
   useRef,
   useState
 } from "react";
+import { Upload } from "antd";
+import { v4 as uuidv4 } from "uuid";
 
 import Wrapper from "primitives/Wrapper";
 
@@ -18,11 +20,23 @@ import {
   SmileOutlined
 } from "@ant-design/icons";
 import FieldUpload, { FileAccept } from "../../primitives/FieldUpload";
+import { ExtendedFile } from "./types";
 
 interface InputMessagePropsInterface {
   defaultInputValue?: string;
   placeholder?: string;
 }
+
+const formatFilesData = (files: File[]): ExtendedFile[] =>
+  files.map((file) => ({
+    name: file.name,
+    size: file.size,
+    type: file.type,
+    uid: uuidv4()
+  }));
+
+const filterFileListById = (id: string) => (prevFileList: ExtendedFile[]) =>
+  prevFileList.filter((file) => file.uid !== id);
 
 const InputMessage: FC<InputMessagePropsInterface> = ({
   placeholder,
@@ -33,6 +47,8 @@ const InputMessage: FC<InputMessagePropsInterface> = ({
   const [inputValue, setInputValue] = useState<string>(defaultInputValue);
 
   const [isFocusInput, setIsFocusInput] = useState(false);
+
+  const [fileList, setFileList] = useState<ExtendedFile[]>([]);
 
   const changeValueInput = useCallback(
     (e: FormEvent<HTMLDivElement>) => {
@@ -49,74 +65,86 @@ const InputMessage: FC<InputMessagePropsInterface> = ({
     // eslint-disable-next-line
   }, []);
 
+  const changeFileList = useCallback((files) => {
+    setFileList(formatFilesData(files));
+  }, []);
+
+  //@ts-ignore
+  const removeFile = (data: ExtendedFile) => {
+    setFileList(filterFileListById(data.uid));
+  };
+
   return (
-    <Wrapper
-      className={classNames(styleModule.input, {
-        [styleModule.input_focused]: isFocusInput
-      })}
-    >
-      <SmileOutlined
-        className={classNames(
-          styleModule.input__smiles,
-          styleModule.icon_common
-        )}
-      />
-      {!!placeholder && !inputValue && (
-        <span
-          onClick={() => inputRef.current && inputRef.current.focus()}
-          className={styleModule.input__placeholder}
-        >
-          {placeholder}
-        </span>
-      )}
-      <div
-        ref={inputRef}
-        contentEditable
-        onInput={changeValueInput}
-        className={styleModule.input__input}
-        onFocus={() => {
-          if (isFocusInput) return;
-          setIsFocusInput(true);
-        }}
-        onBlur={() => {
-          if (!isFocusInput) return;
-          setIsFocusInput(false);
-        }}
-      />
-
-      <div className={styleModule.input__actionMessageWrap}>
-        <FieldUpload
-          onFilesLoaded={(files) => {
-            console.log("filesCheck => ", files);
-          }}
-          view={(openDialog) => {
-            return (
-              <button
-                onClick={openDialog}
-                className={styleModule.input__button}
-              >
-                <PictureOutlined
-                  className={classNames(styleModule.icon_common)}
-                />
-              </button>
-            );
-          }}
-          accept={FileAccept.IMAGE}
-          multiple
+    <>
+      <Wrapper
+        className={classNames(styleModule.input, {
+          [styleModule.input_focused]: isFocusInput
+        })}
+      >
+        <SmileOutlined
+          className={classNames(
+            styleModule.input__smiles,
+            styleModule.icon_common
+          )}
         />
+        {!!placeholder && !inputValue && (
+          <span
+            onClick={() => inputRef.current && inputRef.current.focus()}
+            className={styleModule.input__placeholder}
+          >
+            {placeholder}
+          </span>
+        )}
+        <div
+          ref={inputRef}
+          contentEditable
+          onInput={changeValueInput}
+          className={styleModule.input__input}
+          onFocus={() => {
+            if (isFocusInput) return;
+            setIsFocusInput(true);
+          }}
+          onBlur={() => {
+            if (!isFocusInput) return;
+            setIsFocusInput(false);
+          }}
+        />
+        <div className={styleModule.input__actionMessageWrap}>
+          <FieldUpload
+            onFilesLoaded={changeFileList}
+            view={(openDialog) => {
+              return (
+                <button
+                  onClick={openDialog}
+                  className={styleModule.input__button}
+                >
+                  <PictureOutlined
+                    className={classNames(styleModule.icon_common)}
+                  />
+                </button>
+              );
+            }}
+            accept={FileAccept.IMAGE}
+            multiple
+          />
 
-        <button className={styleModule.input__button}>
-          <AudioOutlined className={classNames(styleModule.icon_common)} />
-        </button>
+          <button className={styleModule.input__button}>
+            <AudioOutlined className={classNames(styleModule.icon_common)} />
+          </button>
 
-        <button
-          className={styleModule.input__button}
-          onClick={() => console.log("click")}
-        >
-          <SendOutlined className={classNames(styleModule.icon_common)} />
-        </button>
-      </div>
-    </Wrapper>
+          <button
+            className={styleModule.input__button}
+            onClick={() => console.log("click")}
+          >
+            <SendOutlined className={classNames(styleModule.icon_common)} />
+          </button>
+        </div>
+      </Wrapper>
+
+      {fileList.length > 0 && (
+        <Upload fileList={fileList} onRemove={removeFile} />
+      )}
+    </>
   );
 };
 
