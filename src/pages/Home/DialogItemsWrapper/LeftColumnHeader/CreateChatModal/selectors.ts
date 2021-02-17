694cecb4f9ef2d2772b5badd315e36113106a6cf
alexandr <bratvailolco@mail.ru>
2021-02-17 04:41:25 +0300

    Добавлено исключение авторизованного пользователя, при создании чата.

--- src/pages/Home/DialogItemsWrapper/LeftColumnHeader/CreateChatModal/selectors.ts
+++ src/pages/Home/DialogItemsWrapper/LeftColumnHeader/CreateChatModal/selectors.ts
@@ -1,8 +1,8 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { prop, uniqBy } from "ramda";
 
-import { StateInterface } from "../../../../../state/store";
-import { ChatInterface, UserInterface } from "../../../../../types/types";
+import { StateInterface } from "state/store";
+import { ChatInterface, UserInterface } from "types/types";
 
 function getUsersIdsByProp(chats: ChatInterface[], prop: keyof ChatInterface) {
   return chats.map((chat) => chat[prop]);
@@ -18,19 +18,27 @@ const selectExistedUsersFromChats = (
   return uniqBy(prop("id"), concatedUsers);
 };
 
+const selectCurrentUser = (state: StateInterface): UserInterface => {
+  return state.userModule.userInfo;
+};
+
 const selectAllUsers = (state: StateInterface): UserInterface[] =>
   state.userModule.allUsers;
 
 const selectExcludeExistedUsersFromAllUsers = (
   allUsers: UserInterface[],
-  excludeUsers: UserInterface[]
+  excludeUsers: UserInterface[],
+  currentUser: UserInterface
 ) => {
   const excludeUsersIds = excludeUsers.map(prop("id"));
-  return allUsers.filter((user) => !excludeUsersIds.includes(user.id));
+  return allUsers.filter(
+    (user) => !excludeUsersIds.includes(user.id) && user.id !== currentUser.id
+  );
 };
 
 export const selectUsersForCreateChat = createSelector(
   selectAllUsers,
   selectExistedUsersFromChats,
+  selectCurrentUser,
   selectExcludeExistedUsersFromAllUsers
 );
