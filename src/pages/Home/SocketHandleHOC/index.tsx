99b68369359c813f8f56bf0053be9e118655da10
alexandr <alexander.avdeev@balance-pl.ru>
2021-04-25 21:22:50 +0300

    Причесывание кода компонента SocketHandleHOC

--- src/pages/Home/SocketHandleHOC/index.tsx
+++ src/pages/Home/SocketHandleHOC/index.tsx
@@ -2,15 +2,15 @@ import React, { FC, memo, useEffect } from "react";
 import { shallowEqual } from "react-redux";
 import io from "socket.io-client";
 
-import { UserInterface } from "types/types";
-
 import { useAppDispatch, useTypedSelector } from "state/store";
-
-import { ChatEvent } from "./types";
 import {
   setUserOfflineById,
   setUserOnlineById
-} from "../../../state/modules/user/actions";
+} from "state/modules/user/actions";
+
+import { UserInterface } from "types/types";
+
+import { ChatEvent } from "./types";
 
 const socket = io(process.env.REACT_APP_SOCKET_URL);
 
@@ -30,18 +30,6 @@ const SocketHOC = ({
   );
 
   useEffect(() => {
-    // socket.on(ChatEvent.NEW_MESSAGE, (msg: MessageInterface) => {
-    //   console.log("new message event ! -> ", msg);
-    // });
-
-    // socket.on("user_leave", (msg: any) => {
-    //   console.log("user leave ", msg);
-    // });
-
-    // socket.on("CHAT:CREATED", (chat: any) => {
-    //   console.log("new chat -> ", chat);
-    // });
-
     socket.on(ChatEvent.USER_ONLINE, (joinedUserData: UserInterface) => {
       if (!currentUserInfo || currentUserInfo?.id === joinedUserData?.id) {
         return;
